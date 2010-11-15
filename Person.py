86288bec6077fc2648e6a45dc4fc6ec6ca5d32ba
MrSuborbian88 <MrSuborbian88@31a00cc1-77ee-1e24-4270-92889f8fca66>
2010-11-15 18:37:09 +0000

    Added Person.py - an object to store comment information

---
+++ Person.py
@@ -0,0 +1,7 @@
+class Commenter:
+	def __init__(self, comment, date, positive, negative, gender, age, location):
+		self.comment = comment
+		self.date = date
+		self.positive = positive
+		self.age = age
+		self.location = location
