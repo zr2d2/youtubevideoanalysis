25397f1052cb0f92fc0f53a5e4d8e1939c76420c
zrowe007 <zrowe007@31a00cc1-77ee-1e24-4270-92889f8fca66>
2010-11-22 01:35:59 +0000

    recommiting Dan's code

--- scrape.php
+++ scrape.php
@@ -1,1 +1,30 @@
-12
+<?php
+include('simple_html_dom.php');
+$file = 'comments2.txt';
+$Handle = fopen($file, 'w');
+for ( $counter = 1; $counter <= 58; $counter += 1) {
+echo $counter;
+$url = 'http://www.youtube.com/comment_servlet?all_comments=1&v=9SnQOdEXbNQ&page=' . $counter;
+$html = file_get_html($url);
+
+foreach($html->find('div.content') as $e) {
+foreach($e->find('a.author') as $td) {
+$data = $data . "\n+_+comment+_+\n";
+$data = $data . htmlspecialchars($td->plaintext) . "\t";
+}
+foreach($e->find('span.time') as $time) {
+$data = $data . htmlspecialchars($time->plaintext) . ' ';
+}
+foreach($e->find('div.comment-text') as $comment) {
+$comment = strip_tags($comment->plaintext);
+$comment = str_replace("»¿", "", $comment);
+$comment = str_replace("ï", "", $comment);
+$comment = str_replace("<br>", "", $comment);
+$comment = str_replace("<wbr>", "", $comment);
+$data = $data . $comment;
+}
+}
+fwrite($Handle, $data); 
+}
+fclose($Handle); 
+?>
